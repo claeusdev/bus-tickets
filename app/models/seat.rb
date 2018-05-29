9cfec51ea74978cd05e2a5d97a36d9db7c615aaa
Nana Adjei Manu <claeusdev@gmail.com>
2018-05-29 09:25:56 +0000

    Ref: fixed ticket show

--- app/models/seat.rb
+++ app/models/seat.rb
@@ -1,3 +1,4 @@
 class Seat < ApplicationRecord
 	belongs_to :bus
+	has_many :tickets, dependent: :destroy
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,4 +2,5 @@ class Ticket < ApplicationRecord
   belongs_to :route
   has_many :orders, dependent: :destroy
   belongs_to :bus
+  belongs_to :seat
 end
