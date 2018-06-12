67e26ef1faf0d6c1cfeaa75d011ac62bb0060343
Nana Adjei Manu <claeusdev@gmail.com>
2018-06-12 17:25:49 +0000

    Try search query fix

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,5 @@
 class SearchesController < ApplicationController
   def search
-    @routes = Route.where('origin iLike? OR destination iLike?',  "%#{params[:from]}%", "%#{params[:destination]}%").order(created_at: :desc)
+    @routes = Route.where('origin iLike? AND destination iLike?',  "%#{params[:from]}%", "%#{params[:destination]}%").order(created_at: :desc)
   end
 end
